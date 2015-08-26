a8c322023fe6ed685fb99d06aacfc06f341c7e7b
sikisuti <sikisuti@gmail.com>
2015-08-26 16:32:47 +0200

    NLog NuGet package added

---
+++ Common/Dependency Injection/DIContainer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Ninject;
+
+namespace Common.Dependency_Injection
+{
+    public sealed class DIContainer
+    {
+        private StandardKernel kernel;
+        private DIContainer instance = new DIContainer();
+
+        private DIContainer()
+        {
+            this.kernel = new StandardKernel();
+        }
+
+        public DIContainer Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public T Get<T>()
+        {
+            return this.kernel.Get<T>();
+        }
+    }
+}
